45ad157882fbea3e9ce53dc71a8424f1306555d0
Steve L. Nyemba -- The Architect <steve@the-phi.com>
2018-10-31 13:31:28 -0500

    setup file to build the egg

--- src/setup.py
+++ src/setup.py
@@ -3,5 +3,12 @@ from setuptools import setup, find_packages
 setup(
     name = "deid",
     version = "0.1",
+    author = "Steve L. Nyemba",
+    author_email = "steve@the-phi.com",
+    description = ("An demonstration of how to create, document, and publish "
+                                   "to the cheese shop a5 pypi.org."),
+    license = "BSD",
+    keywords = "example documentation tutorial",
+    url = "http://packages.python.org/an_example_pypi_project",
     packages = find_packages()
     )
