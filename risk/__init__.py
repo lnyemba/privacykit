bd7348fe20ce0d76fec9b2befcf73561948a661a
Steve Nyemba <steve@the-phi.com>
2019-03-05 15:31:14 -0600

    fixing documentation

--- risk/__init__.py
+++ risk/__init__.py
@@ -1,51 +1,69 @@
 """
-(c) 2019, Health Information Privacy Lab
-Brad. Malin, Weiyi Xia, Steve L. Nyemba
+# Re-Identification Risk
 
-This framework computes re-identification risk of a dataset assuming the data being shared can be loaded into a dataframe (pandas)
-The framework will compute the following risk measures:
-    - marketer
-    - prosecutor
-    - pitman
+This framework computes re-identification risk of a dataset by extending pandas. It works like a pandas **add-on** 
+The framework will compute the following risk measures: marketer, prosecutor, journalist and pitman risk. 
+References for the risk measures can be found on 
+ - http://www.ehealthinformation.ca/wp-content/uploads/2014/08/2009-De-identification-PA-whitepaper1.pdf
+ - https://www.scb.se/contentassets/ff271eeeca694f47ae99b942de61df83/applying-pitmans-sampling-formula-to-microdata-disclosure-risk-assessment.pdf
 
-References :
-    https://www.scb.se/contentassets/ff271eeeca694f47ae99b942de61df83/applying-pitmans-sampling-formula-to-microdata-disclosure-risk-assessment.pdf
+There are two modes available :
+    
+**explore:**
 
-This framework integrates pandas (for now) as an extension and can be used in two modes :
-1. explore:
-    Here the assumption is that we are not sure of the attributes to be disclosed, 
-    The framework will explore a variety of combinations and associate risk measures every random combinations it can come up with
+Here the assumption is that we are not sure of the attributes to be disclosed, the framework will randomly generate random combinations of attributes and evaluate them accordingly as it provides all the measures of risk. 
 
-2. evaluation
-    Here the assumption is that we are clear on the sets of attributes to be used and we are interested in computing the associated risk.
+**evaluation**
+
+Here the assumption is that we are clear on the sets of attributes to be used and we are interested in computing the associated risk.
 
 
-Four risk measures are computed :
+### Four risk measures are computed :
+
     - Marketer risk
     - Prosecutor risk
     - Journalist risk
     - Pitman Risk
 
-Usage:
-import numpy as np
-import pandas as pd
-from pandas_risk import *
+### Usage:
 
-mydf = pd.DataFrame({"x":np.random.choice( np.random.randint(1,10),50),"y":np.random.choice( np.random.randint(1,10),50) })
-print mydf.risk.evaluate()
+Install this package using pip as follows :
+
+Stable :
+    
+    pip install git+https://hiplab.mc.vanderbilt.edu/git/steve/deid-risk.git
+    
+    
+Latest Development (not fully tested):
+    
+    pip install git+https://hiplab.mc.vanderbilt.edu/git/steve/deid-risk.git@risk
+    
+The framework will depend on pandas and numpy (for now). Below is a basic sample to get started quickly.
+
+
+    import numpy as np
+    import pandas as pd
+    from pandas_risk import *
+
+    mydf = pd.DataFrame({"x":np.random.choice( np.random.randint(1,10),50),"y":np.random.choice( np.random.randint(1,10),50),"z":np.random.choice( np.random.randint(1,10),50),"r":np.random.choice( np.random.randint(1,10),50)  })
+    print mydf.risk.evaluate()
 
 
 
-#
-# computing journalist and pitman
-#   - Insure the population size is much greater than the sample size 
-#   - Insure the fields are identical in both sample and population
-#
-pop = pd.DataFrame({"x":np.random.choice( np.random.randint(1,10),150),"y":np.random.choice( np.random.randint(1,10),150) ,"q":np.random.choice( np.random.randint(1,10),150)})
-mydf.risk.evaluate(pop=pop)
+    #
+    # computing journalist and pitman
+    #   - Insure the population size is much greater than the sample size 
+    #   - Insure the fields are identical in both sample and population
+    #
+    pop = pd.DataFrame({"x":np.random.choice( np.random.randint(1,10),150),"y":np.random.choice( np.random.randint(1,10),150) ,"z":np.random.choice( np.random.randint(1,10),150),"r":np.random.choice( np.random.randint(1,10),150)})
+    mydf.risk.evaluate(pop=pop)
+
 
 @TODO:
     - Evaluation of how sparse attributes are (the ratio of non-null over rows)
     - Have a smart way to drop attributes (based on the above in random policy search)
+Basic examples that illustrate usage of the the framework are in the notebook folder. The example is derived from 
+
+	
 """
 from risk import risk
